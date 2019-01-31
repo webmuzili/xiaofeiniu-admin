83854377e2804e92440a72c6a705ff0fa2c2e621
liyuan <1250953354@qq.com>
2019-02-01 00:26:52 +0800

    Vue组件的多样式

--- src/views/TableList.vue
+++ src/views/TableList.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="home">
-      <h3>这是list组件</h3>
+  <div class="xfn-table-list">
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>桌台管理</el-breadcrumb-item>
+      <el-breadcrumb-item>桌台列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <br>
+    <el-row>
+      <el-col v-for="(t,i) in tableList" :key='i' :xs="12" :sm="8" :md="6" :lg="4" :xl="2">
+        <xfn-table :data="t"></xfn-table>
+      </el-col>
+    </el-row>  
   </div>
 </template>
 
 <script>
-
+import Table from '../components/Table'
 export default {
-
+  data(){
+    return {
+      tableList: []
+    }
+  },
+  components: {
+    'xfn-table': Table
+  }
 }
 </script>
